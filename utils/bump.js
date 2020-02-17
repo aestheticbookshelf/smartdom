35dba814926baacced03c49e2b9a014948639080
aestheticbookshelf <aestheticbookshelfgmail.com>
2020-02-17 15:49:15 +0100

    Fix bump

--- utils/bump.js
+++ utils/bump.js
@@ -7,3 +7,5 @@ let m = pkgJson["version"].match(/(.*\.)([0-9]+)$/)
 let newVer = m[1] + ( parseInt(m[2]) + 1 )
 
 pkgJson["version"] = newVer
+
+fs.writeFileSync("package.json", JSON.stringify(pkgJson, null, 2))
